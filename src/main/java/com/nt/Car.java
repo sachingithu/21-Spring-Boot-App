f13717f724b4b7c6a8d5991006f7ca001edaa88b
Pratiksha <prati.jadhav03@gmail.com>
2023-06-27 16:24:21 +0530

    After stash done exp

--- src/main/java/com/nt/Car.java
+++ src/main/java/com/nt/Car.java
@@ -1,10 +1,14 @@
 package com.nt;
 
 public class Car {
+	public void start() {
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+//changes for HIS122
+		int i=10;
 	}
 
 }
