954a2da34f9e2f3586a1f38898c854582a38b3a6
Pratiksha <prati.jadhav03@gmail.com>
2023-06-27 16:06:39 +0530

    changes commited of HSI 132

--- src/main/java/com/nt/Application.java
+++ src/main/java/com/nt/Application.java
@@ -10,6 +10,7 @@ public class Application {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext run=SpringApplication.run(Application.class, args);
 		run.close();
+		//HSI 132
 	}
 
 }
